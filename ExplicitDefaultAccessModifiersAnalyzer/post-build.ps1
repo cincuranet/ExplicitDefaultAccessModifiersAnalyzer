ed36d8b8cee95e9081cfee0a3e1a3b702e967486
Jiri Cincura <jiri@x2develop.com>
2015-11-13 06:24:19 +0100

    Fixed filename in script.

--- ExplicitDefaultAccessModifiersAnalyzer/post-build.ps1
+++ ExplicitDefaultAccessModifiersAnalyzer/post-build.ps1
@@ -1,5 +1,5 @@
 try {
-	$version = (gi ConfigureAwaitChecker.dll).VersionInfo.ProductVersion
+	$version = (gi ExplicitDefaultAccessModifiersAnalyzer.dll).VersionInfo.ProductVersion
 	echo "Building package with version $version."
 	& '..\..\..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe' pack ExplicitDefaultAccessModifiersAnalyzer.nuspec -Version $version -OutputDirectory .
 }
