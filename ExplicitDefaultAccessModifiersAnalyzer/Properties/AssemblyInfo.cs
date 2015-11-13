2e59ccf05f2a9f8770f80348bf1bb03b8b1f7ea0
Jiri Cincura <jiri@x2develop.com>
2015-11-13 06:45:42 +0100

    Correct AssemblyInfo.

--- ExplicitDefaultAccessModifiersAnalyzer/Properties/AssemblyInfo.cs
+++ ExplicitDefaultAccessModifiersAnalyzer/Properties/AssemblyInfo.cs
@@ -1,34 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ExplicitDefaultAccessModifiersAnalyzer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jiri {x2} Cincura")]
 [assembly: AssemblyProduct("ExplicitDefaultAccessModifiersAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright (c) Jiri {x2} Cincura")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("ExplicitDefaultAccessModifiersAnalyzer.Tests")]
+[assembly: Guid("8AD23C8D-47B9-4343-BAF8-20C1ACBB7DDF")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
