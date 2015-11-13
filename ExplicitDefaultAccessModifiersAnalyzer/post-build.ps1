b16696213e318851351930dc7fd20065fbacd783
Jiri Cincura <jiri@x2develop.com>
2015-11-13 06:19:24 +0100

    Building the package.

---
+++ ExplicitDefaultAccessModifiersAnalyzer/post-build.ps1
@@ -0,0 +1,8 @@
+try {
+	$version = (gi ConfigureAwaitChecker.dll).VersionInfo.ProductVersion
+	echo "Building package with version $version."
+	& '..\..\..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe' pack ExplicitDefaultAccessModifiersAnalyzer.nuspec -Version $version -OutputDirectory .
+}
+catch {
+	exit 1
+}
